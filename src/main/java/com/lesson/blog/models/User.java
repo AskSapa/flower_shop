3353f319c4a37a0f75d5df14aff8f5bc5da29b7f
AskSapa <askat.daniarov03@gmail.com>
2023-04-03 15:13:44 +0600

    проверено

--- src/main/java/com/lesson/blog/models/User.java
+++ src/main/java/com/lesson/blog/models/User.java
@@ -3,7 +3,8 @@ package com.lesson.blog.models;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+//TODO: соблюдай SOLID
+//FIXME: Исправь метод salary
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
